b529057b6cd6819e280f8af7179423dba772a52b
edsoglez <edsoglez@gmail.com>
2023-09-06 16:08:44 -0600

    added login functionality

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 import {getDatabase, set, get, update, remove, ref, child, onValue} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
-        submitButton.addEventListener('click',findUser);
+    
+      
 
+    submitButton.addEventListener('click',findUser);
         function findUser(){
+            window.fieldUsername = document.querySelector("#enterUsername");
+            window.fieldPassword = document.querySelector("#enterPassword");
             const dbref = ref(db)
             get(child(dbref,'Users/'+fieldUsername.value))
             .then((snapshot)=>{
                 if(snapshot.exists()){
                     if(snapshot.val().password == fieldPassword.value){
-                        alert('Correct user and password')
+                        location.href = 'order.html';
                     }
                     else{
                         alert('invalid password')
+                        fieldUsername.value = ""
+                        fieldPassword.value = ""
                     }
                 }
                 else{   
                     alert('user not found')
+                    fieldUsername.value = ""
+                    fieldPassword.value = ""
                 }   
             })
+            
         }
 
