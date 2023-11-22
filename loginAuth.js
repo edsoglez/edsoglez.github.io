c3364d546ed3feca34efb70f44fc7ebfe4967ada
edsoglez <edsoglez@gmail.com>
2023-11-22 11:24:52 -0600

    login

---
+++ loginAuth.js
@@ -0,0 +1,19 @@
+import {getDatabase, set, get, update, remove, ref, child, onValue} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js"; 
+
+export function Auth(_username,_password){
+    console.log(username,password)
+
+    get(child(db, 'Users/'+_username)).then((snapshot) => {
+        if (snapshot.exists()) {
+            //if(snapshot.val().password == _password){
+                alert("passed")
+            //}
+
+        } else {
+            console.log("No user found");
+        }
+    });
+    
+}
+
+window.Auth = Auth;
