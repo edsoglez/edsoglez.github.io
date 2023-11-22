9129807931d3a03c5ee9182adb8497b11643f03b
edsoglez <edsoglez@gmail.com>
2023-11-22 11:27:36 -0600

    ddd

--- loginAuth.js
+++ loginAuth.js
@@ -1,9 +1,8 @@
 import {getDatabase, set, get, update, remove, ref, child, onValue} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js"; 
 
 export function Auth(_username,_password){
-    console.log(username,password)
-
-    get(child(db, 'Users/'+_username)).then((snapshot) => {
+  
+    get(child(db,`Users/`+_username)).then((snapshot) => {
         if (snapshot.exists()) {
             //if(snapshot.val().password == _password){
                 alert("passed")
