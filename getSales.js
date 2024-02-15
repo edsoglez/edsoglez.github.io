13c072f6b8f7fa472572eed64d5233ef7e28ca40
edsoglez <edsoglez@gmail.com>
2024-02-15 13:07:01 -0600

    a

--- getSales.js
+++ getSales.js
@@ -166,6 +166,7 @@ function drawChart() {
     
      // Instantiate and draw our chart, passing in some options.
     var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
      
     chart.draw(data, options);
     }, "1000");
