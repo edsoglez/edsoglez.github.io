8a9da3f5c7e0c57dee67ced82232082950b6b478
edsoglez <edsoglez@gmail.com>
2024-02-20 17:43:31 -0600

    a

--- sell.js
+++ sell.js
@@ -284,7 +284,7 @@ function getCorte(){
             Restante: (Number(salesTotalCash) + Number(gastosTotal))
         });
 
-        localStorage.href="index.html"
+        location.href="index.html"
         })
     })
 
