4c578ff678e57089b9067f3c7d69f9b4745a81f2
edsoglez <edsoglez@gmail.com>
2024-04-30 14:37:12 -0600

    get past corte data

--- sell.js
+++ sell.js
@@ -273,6 +273,9 @@ function getCorte(){
     let fromDateSerial = Number(fromDate.replace(/-/g,""))
     let toDateSerial = Number(toDate.replace(/-/g,""))
 
+    let pastCorte = get(child(ref(db),'Cortes/'+new Date().getFullYear()+"/"+(new Date().getMonth()+1)+"/"+ new Date().toISOString().replace(/\D/g,'_').substring(8,10)))
+    console.log(pastCorte)
+
     get(child(ref(db),'Sales/')).then((snapshot) => {
         //sets sum to zero
         let salesTotal = 0;
