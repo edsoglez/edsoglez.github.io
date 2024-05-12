d3af07388851db873a01d6398827a44dce6da595
edsoglez <edsoglez@gmail.com>
2024-05-11 20:10:12 -0600

    removed ISO date reference

--- pay.js
+++ pay.js
@@ -7,8 +7,12 @@ function registerExpense(){
         if(amount===NaN){alert("Ingresaste un valor no numerico"); return}
         if(amount==null || amount == ""){alert("Gasto no registrado"); return}
 
-    let dateFormatedID = new Date().toISOString().replace(/\D/g,'').substring(0,8)+new Date().toTimeString().replace(/\D/g,'');
-    //Example of format YYYYMMDDHHMMSS
+        let month = String(new Date().getUTCMonth()+1).padStart(2, '0')
+        let date = String(new Date()).split(" ")
+        let day = String(new Date().getDate()).padStart(2,'0')
+        let year = new Date().getUTCFullYear()
+        //on page load reset order to 0 and render product cards
+        let dateFormatedID = year+month+day+new Date().toTimeString().replace(/\D/g,'');    //Example of format YYYYMMDDHHMMSS
 
     set(ref(db,'Gastos/'+new Date().getFullYear()+"/"+(new Date().getMonth()+1)+"/"+ dateFormatedID),{
         Time: String(new Date()).substring(16,24),
